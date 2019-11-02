27daa54702b19313956dbad73e6a0f2ee9768d9d
Nathan Brickett <natey37@gmail.com>
2019-11-02 16:54:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  array.each {|x| square_array << x*x}
+  square_array
 end
+
+def square_array_map(array)
+  square_array = array.map {|x| x*x}
+  square_array
+end 
